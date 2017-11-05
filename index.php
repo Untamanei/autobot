4ea9e0ef506343e0606ab0649176f2370cdcec28
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-11-05 16:27:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
        $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
        
        $textMessageBuilder = new TextMessageBuilder($respMessage);
-       $response = $bot->replyMessage($replyToken, $textMessageBuilder); } } echo "
+       $response = $bot->replyMessage($replyToken, $textMessageBuilder);
        
    }
 }    

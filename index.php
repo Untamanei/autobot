837bfd7c7cb0786307c09ddb6323ff6549bbca5d
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-11-06 10:18:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+// use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 
 $channel_token = 'qkemiHYLq1Go2vUvCixhJFKeTfxGgxd57LkiruFfPV58idgTkUbL7Tn7HUtluFRI8Q/nAbQdyX+yPeOtQ/GcvQQFH122gT6Lu0eqtci22BQ2/doT8zSakFQ7jLIfeakP0NGR6NLOqMZPM+y71siy6wdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'e60fa284733b2a846a6309e8bc3df90e';
@@ -30,11 +30,11 @@ if (!is_null($events['events'])) {
 	    $ans = $event['message']['text'];
 			
             switch(strtolower($ans)) {          
-                case 'tel':
-                    $originalContentUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
-		    $previewImageUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
-	            $textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
-                    break;
+                //case 'tel':
+                    //$originalContentUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+		    //$previewImageUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+	            //$textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
+                    //break;
                 case 'address':
                     $respMessage = '99/451 Muang Nonthaburi';
                     break;

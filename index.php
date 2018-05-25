af79dec19f706e44d0aa23e8e692a1b6df103092
Untamanei <31429630+Untamanei@users.noreply.github.com>
2018-05-25 16:37:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,20 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
+if($arrJson['events'][0]['message']['text'] == "Wi Boiler"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "1.เปิดระบบไฟฟ้า\n2.นำสาย Flexible Hose มาต่อเข้ากับหัว Quick Connect\n3.เปิดโปรแกรม LabView เพื่อเปิด Solenoid Valve\4.เปิดระบบปั๊มน้ำ Feed\5.เปิดเครื่อง Boiler\n6.รอ Boiler ทำ Pressure ได้ตามที่ตั้งไว้\n7.เปิดวาล์วทางด้าน Steam เพื่อนำไปใช้งานต่อไป\n";
+  }
 
+else if($arrJson['events'][0]['message']['text'] == "Pid boiler"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "image";
+  $arrPostData['messages'][0]['originalContentUrl'] = "https://image.goosiam.com/imgupload/upload71/UD7JU0BieuuO.jpg";
+  $arrPostData['messages'][0]['previewImageUrl'] = "https://image.goosiam.com/imgupload/upload71/UD7JU0BieuuO.jpg";
+  }
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
